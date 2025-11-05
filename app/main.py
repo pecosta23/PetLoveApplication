65f4364dd645b912f810196372adf65b19f0da4a
Pedro Costa <pedrocosta2802@gmail.com>
2025-11-05 17:34:28 -0300

    feat - model configs

--- app/main.py
+++ app/main.py
@@ -15,7 +15,41 @@ app = FastAPI(title="PetLove API")
 class Question(BaseModel):
     question: str = Field(..., min_c=1, max_c=2000)
 
-class Response(BaseModel):
+class Answer(BaseModel):
     response: str
+
+@app.post("/api/question-and-answer", response_model=Answer)
+async def q_and_a(req:Question):
+    if not KEY:
+        raise HTTPException(status_code=500, detail="Your API key is not configured")
+    
+    # writing sys message
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                "Você é o melhor assistente de vendas dentro de um e-comerce de pets de nome PetLove."
+                "É crucial que você responda a cada pergunta de forma clara e educada. E quando você identificar o momento correto, suriga produtos do nosso catálogo (rações, acessórios e brinquedos)."
+                "Quando for recomendar qualquer tipo de ração pergunte por mais informações como: idade e porte do animal assim como possíveis alergias."
+            ),
+        },
+        {"role": "user", "content": req.question}
+    ]
+
+    payload = {
+        "model": MODEL,
+        "messages": messages,
+        "temperature": 0.2,
+        "max_tokens": 500,
+    }
+
+    headers = {
+        "Authorization": f"Bearer {KEY}",
+        "Content-Type": "application/json",
+    }
+
+    
+
+
 
 
